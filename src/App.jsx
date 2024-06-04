cfc41b94e6066a436eb565afa68d70a1aa20bbeb
Johan Chereau <johanchereau2@gmail.com>
2024-06-04 21:23:22 +0200

    feat : - Creation of an error handling page - Redirect to error page in case of error

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Root from './pages/Root';
 import HomePage from './pages/HomePage';
+import ErrorPage from './pages/ErrorPage';
 
 const App = () => {
   const router = createBrowserRouter([
     {
       path: '/',
       element: <Root />,
+      errorElement: <ErrorPage />,
       children: [
         {
           path: '',
