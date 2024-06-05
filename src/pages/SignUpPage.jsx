4a1a9f8654adf1eb1b5d31d27ada969df32f25dc
Johan Chereau <johanchereau2@gmail.com>
2024-06-05 16:36:50 +0200

    feat: creating a ui component for background mesh

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -15,6 +15,7 @@ import {
 import { Input } from '@/components/ui/Input';
 import { Checkbox } from '@/components/ui/Checkbox';
 import { Link } from 'react-router-dom';
+import BackgroundMesh from '@/components/ui/BackgroundMesh';
 
 const FormSchema = z
   .object({
@@ -82,7 +83,7 @@ const SignUpPage = () => {
   return (
     <section className="py-20 grid grid-flow-row gap-16 place-items-center w-full">
       <div className="text-center relative">
-        <div className="absolute w-1/4 aspect-square bg-primary top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[-1] rounded-full blur-[100px] opacity-65"></div>
+        <BackgroundMesh />
         <h1 className="text-3xl md:text-6xl font-bold">Sign up</h1>
         <p className="text-muted-foreground mt-4 md:text-xl">
           Enter your information to create an account.
