2e39a164b0bf5bf142bbd2a868af642f0fafc3c6
Vincent Vignaud <vincentvignaud@outlook.fr>
2024-06-06 15:36:45 +0200

    Card done

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Badge = ({rating, Icon}) => {
 
   return (
-    <div className="container w-fit border border-white rounded">
+    <div className="w-fit border border-white rounded">
     <div className='inline-flex gap-5 items-center'>
       {rating} <Icon/>
     </div> 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,13 @@
-import Card from "@/components/Card";
-
 const HomePage = () => {
 
   return (<div className="min-h-svh grid grid-rows-[auto_1fr_auto]">
 
     
   <main className="container">
-  <Card />
+
   </main>
 
-  
+
 </div>
 )
 };
